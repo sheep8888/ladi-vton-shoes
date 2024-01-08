8b355dbc91428179624b4510473028acebfd6bb2
mortal-163 <155989908+mortal-163@users.noreply.github.com>
2024-01-09 00:23:42 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -62,3 +62,13 @@ def warping_module(dataset: Literal['dresscode', 'vitonhd']):
     refinement.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint_url, map_location='cpu')['refinement'])
 
     return tps, refinement
+
+def warping1_module(dataset: Literal['dresscode', 'vitonhd']):
+    tps = ConvNet_TPS(256, 192, 21, 3)
+    refinement = UNetVanilla(n_channels=24, n_classes=3, bilinear=True)
+
+    checkpoint_url = f"https://github.com/mortal-163/ladi-vton/releases/download/weights/checkpoint_last.pth"
+    tps.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint_url, map_location='cpu')['tps'])
+    refinement.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint_url, map_location='cpu')['refinement'])
+
+    return tps, refinement
